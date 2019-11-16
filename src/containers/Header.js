242ed921242406e037f35c0790ed9c29fb256c5c
sanvean74 <dlambertpdx@gmail.com>
2019-11-15 22:32:48 -0800

    switched to redux hooks instead of connect

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,52 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
 import { getFiles } from '../selectors/headerSelectors';
 import { addFile, deleteTabs } from '../actions/headerActions';
+import { useDispatch, useSelector } from 'react-redux';
 import Form from '../components/form/Form';
 import Tabs from '../components/tabs/Tabs';
 import { switchTabs } from '../actions/documentActions';
+import styles from './Header.css';
 
-const Header = ({ tabs, handleSubmit, changeTabs, deleteTab }) => {
+const Header = () => {
+
+  const dispatch = useDispatch();
+  const tabs = useSelector(state => getFiles(state));
+  const handleSubmit = title => dispatch(addFile(title));
+  const changeTabs = title => dispatch(switchTabs(title));
+  const deleteTab = title => dispatch(deleteTabs(title));
+
+
   return (
-    <>
+    <div className={styles.Header}>
       <h1>Markdown Editor</h1>
-      <Form handleSubmit={handleSubmit}/>
+      <Form handleSubmit={handleSubmit} />
       <Tabs tabs={tabs} changeTabs={changeTabs} deleteTab={deleteTab} />
-    </>
+    </div>
   );
-};
+}
 
 Header.propTypes = {
   tabs: PropTypes.array,
   handleClick: PropTypes.func,
-  title: PropTypes.string.isRequired,
+  title: PropTypes.string,
   handleChange: PropTypes.func,
   handleSubmit: PropTypes.func,
-  markdown: PropTypes.string, 
+  markdown: PropTypes.string,
   changeTabs: PropTypes.func,
   deleteTab: PropTypes.func
 };
 
-const mapStateToProps = state => ({
-  tabs: getFiles(state)
-});
-
-const mapDispatchToProps = dispatch => ({
-  handleSubmit(title) {
-    dispatch(addFile(title));
-  },
-  changeTabs(title) {
-    dispatch(switchTabs(title));
-  },
-  deleteTab(title) {
-    dispatch(deleteTabs(title));
-  }
-}); 
-
-const HeaderContainer = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Header);
-
-export default HeaderContainer;
+export default Header;
